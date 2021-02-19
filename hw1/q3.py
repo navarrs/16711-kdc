eabd984a41f0a4f92d798d5f239ab2a52c349657
navarrs <ingrid.navarro.an@gmail.com>
2021-02-19 13:59:44 -0600

    started: FK

--- hw1/q3.py
+++ hw1/q3.py
@@ -1,43 +1,42 @@
 import matplotlib.pyplot as plt
-import numpy as np 
+import numpy as np
 
 from mpl_toolkits.mplot3d import Axes3D
 from numpy.linalg import matrix_power
+from scipy import linalg
 
 np.set_printoptions(4, suppress=True)
 
-# Transformations between frames
+# ------------------------------------------------------------------------------
+# Load data
+
 # G base wrt world
-G_base_world = np.array(
-    [[1.0, 0.0, 0.0,  750.0],
-     [0.0, 1.0, 0.0,  500.0],
-     [0.0, 0.0, 1.0, 1000.0],
-     [0.0, 0.0, 0.0,    1.0]], dtype=np.float)
+G_world_base = np.loadtxt(
+    "data/g_world_base.txt", delimiter=" ", dtype=np.float)
+print(f"G base wrt world:\n{G_world_base}")
 
 # G shoulder wrt base
-G_shl_base = np.array(
-    [[1.0, 0.0, 0.0, 220.0],
-     [0.0, 1.0, 0.0, 140.0],
-     [0.0, 0.0, 1.0, 346.0],
-     [0.0, 0.0, 0.0,   1.0]], dtype=np.float)
+G_base_shoulder = np.loadtxt(
+    "data/g_base_shoulder.txt", delimiter=" ", dtype=np.float)
+print(f"G shoulder wrt base:\n{G_base_shoulder}")
 
-G_shl_world = G_shl_base @ G_base_world
+# G shoulder wrt world
+G_world_shoulder = G_world_base @ G_base_shoulder
+print(f"G shoulder wrt world:\n{G_world_shoulder}")
 
-# G end-effector wrt shoulder
-G_eef_shl = np.array(
-    [[1.0, 0.0, 0.0,   0.0],
-     [0.0, 1.0, 0.0,   0.0],
-     [0.0, 0.0, 1.0, 910.0],
-     [0.0, 0.0, 0.0,   1.0]], dtype=np.float)
+# G tool wrt shoulder
+G_shoulder_tool = np.loadtxt(
+    "data/g_shoulder_tool.txt", delimiter=" ", dtype=np.float)
+print(f"G tool wrt shoulder:\n{G_shoulder_tool}")
 
-# G tool wrt end-effector
-G_tool_eef = np.array(
-    [[1.0, 0.0, 0.0,  0.0],
-     [0.0, 1.0, 0.0,  0.0],
-     [0.0, 0.0, 1.0, 120.0],
-     [0.0, 0.0, 0.0,  1.0]], dtype=np.float)
+# q and w vector to compute the twists
+qw = np.loadtxt("data/qw.txt", delimiter=' ', dtype=np.float)
+print(f"q and w vectors:\n{qw}")
 
-# print(G_world_base @ G_base_shl @ G_shl_end @ G_end_tool)
+joint_data = np.loadtxt("data/joint_data.txt", delimiter=' ', dtype=np.float)
+
+# ------------------------------------------------------------------------------
+# Methods
 
 def compute_twists(qw_matrix):
     twists = np.zeros((len(qw_matrix), 6), dtype=np.float)
@@ -47,72 +46,115 @@ def compute_twists(qw_matrix):
         w = qw[3:]
         twists[i, :3] = -np.cross(w, q)
         twists[i, 3:] = w
-        # print(f"twist: {twists[i]} q: {q} w: {w}")  
     return twists
 
+
 def skew(w):
-    return np.array([[   0,  w[2], -w[1]],
+    return np.array([[0,  w[2], -w[1]],
                      [-w[2],     0, w[0]],
-                     [ w[1], -w[0],   0]], dtype=np.float)
+                     [w[1], -w[0],   0]], dtype=np.float)
+
 
 def compute_matrix_exponential_w(w_hat, theta):
-    I = np.identity(3)
-    # return I + w_hat * np.sin(theta) + w_hat @ w_hat * (1 - np.cos(theta))   
-    return I + w_hat * np.sin(theta) + matrix_power(w_hat, 2) * (1 - np.cos(theta))    
+    return (
+        np.identity(3) 
+        + w_hat * np.sin(theta) 
+        + matrix_power(w_hat, 2) * (1 - np.cos(theta))
+    )
 
-def compute_matrix_exponential_xi(twist, theta):
+
+def compute_matrix_exponential_xi(twist, w_hat, theta):
     I = np.identity(3)
     exp_xi = np.identity(4)
-    
+
     # w, q
     v = twist[:3]
     w = twist[3:]
-    
-    # compute the rotation part 
-    w_hat = skew(w)
+
+    # compute the rotation part
     R = compute_matrix_exponential_w(w_hat, theta)
     exp_xi[:3, :3] = R
-    
+
     # compute the translation part
     # (I - exp_w) (wxv) + wwtvtheta = (I - exp_w) wxv
     w_x_v = np.cross(w, v)
-    t = (I - R) @ w_hat @ v
+    t = (I - R) @ w_x_v
     exp_xi[:3, -1] = t
     return exp_xi
 
-qw = np.loadtxt("qw.txt", delimiter=',', dtype=np.float)
-joint_data = np.loadtxt("JointData.txt", delimiter=' ', dtype=np.float)
-twists = compute_twists(qw)
-n_twists = len(twists)
+# ------------------------------------------------------------------------------
+# Main Program
 
-# G = np.identity(4)
-# for n in range(n_twists):
-#     exp_xi = compute_matrix_exponential_xi(twists[n], 0)
-#     G = G @  exp_xi
-# G_eef_shl = G @ G_eef_shl
-# print(G_tool_eef @ G_eef_shl @ G_shl_base @ G_base_world)
-
-coords = np.zeros((len(joint_data), 3), dtype=np.float)
-for j, joints in enumerate(joint_data):
+def find_normal(A, b):
+    m = A.shape[0]
+    n = A.shape[1]
+    U, s, V_T = linalg.svd(A)
+    S = linalg.diagsvd(s, m, n)
     
-    # find G_eef wrt shouler
-    G_eef_shl_ = np.identity(4)
-    for i, joint_angle in enumerate(joints):
-        xi_exp = compute_matrix_exponential_xi(twists[i], joint_angle)
-        G_eef_shl_ = G_eef_shl_ @ xi_exp
+    k = np.linalg.matrix_rank(S)
+    U_T = np.transpose(U)
+    
+    S_inv = S
+    for i in range(k):
+        S_inv[i, i] = 1 / S_inv[i, i]
+    
+    S_inv = S_inv.transpose()
+    V = np.transpose(V_T)
+
+    # x = V 1/S U_T b
+    x = np.dot(np.matmul(np.matmul(V, S_inv), U_T), b)
+    return x
+    
+
+def main():
+    twists = compute_twists(qw)
+    n_twists = len(twists)
+    
+    w_hats = np.zeros(shape=(n_twists, 3, 3), dtype=np.float)
+    for n in range(n_twists):
+        w_hats[n] = skew(twists[n, 3:])
+    
+    print(f"Twists:\n{twists}")
+
+    coords = np.zeros((len(joint_data), 3), dtype=np.float)
+    for j, joints in enumerate(joint_data):
+
+        # Compute matrix exponentials 
+        xi_exp = np.identity(4)
+        for i, joint_angle in enumerate(joints):
+            xi_exp = xi_exp @ compute_matrix_exponential_xi(
+                twists[i], w_hats[i], joint_angle)
+            
+        # Compute G tool wrt shoulder 
+        G_shoulder_tool_ = xi_exp @ G_shoulder_tool
         
-    # G_tool_world
-    G_tool_world = G_tool_eef @ G_eef_shl_ @ G_shl_world
+        # Compute G tool wrt world 
+        G_world_tool = G_world_shoulder @ G_shoulder_tool_
+
+        # Get tool coordinate 
+        coords[j] = G_world_tool[:3, -1]
+        
+    # Find normal vector
+    # A = np.ones(shape=coords.shape, dtype=np.float)
+    # A[:, 0] = coords[:, 0]
+    # A[:, 1] = coords[:, 1]
+    # b = coords[:, 2]
+    # n = find_normal(A, b)
+    # print(f"Plane normal: {n}")
     
-    coords[j, 0] = G_tool_world[0, 3] # x
-    coords[j, 1] = G_tool_world[1, 3] # y 
-    coords[j, 2] = G_tool_world[2, 3] # z
-print(coords.shape)
-#     print(x[-1], y[-1], z[-1])
-    
-np.savetxt("marker_coords.txt", coords, fmt="%10.5f", delimiter=' ')
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot3D(coords[:, 0], coords[:, 1], coords[:, 2])
-plt.savefig("marker_drawing.png")
-plt.show()
+
+    # Save tool trajectory
+    np.savetxt("marker_coords.txt", coords, fmt="%10.5f", delimiter=' ')
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot3D(coords[:, 0], coords[:, 1], coords[:, 2])
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+    ax.set_title("robot_drawing")
+    plt.savefig("marker_drawing.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
